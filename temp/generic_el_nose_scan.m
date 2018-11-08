38b14a515de1d91df48b60a22d819646e64a0cee
karpenka <osnerde@gmail.com>
2018-11-08 16:06:52 +0300

    ++

--- temp/generic_el_nose_scan.m
+++ temp/generic_el_nose_scan.m
@@ -1,6 +1,30 @@
-focal_dist_in_h = 1, focal_dist_out_h = 1,focal_dist_in_w = 1,  focal_dist_out_w = 1,guide_length = 10, nose_length = 3, m_side = 2, m_top = 6, focal_dist = 0.5, sample_height = 0.01
-[p,m]=mcstas('generic_straight.instr',struct('guide_length', 12, 'm_side', m_min:m_step:m_max, 'm_top', 6,...
-    'sample_height', 0.01, 'sample_width', 0.01,...
-    'guide_start_width',0.03, 'guide_start_height',0.1, 'source_lambda_min',4.85, 'source_lambda_max',4.95,...
-    'cold_regime',1),struct('ncount',1e5));
+R_min = 10; R_step = 10; R_max = 310;
+model = mccode('../generic_guides/generic_half_elliptic_nose.instr');
+name = 'H3-0-TAS';
+parameters.sample_width=0.03;
+parameters.sample_height=0.1;
+parameters.guide_start_width=0.03;
+parameters.guide_start_height=0.1;
+parameters.source_lambda_min=4.85;
+parameters.source_lambda_max=4.95;
+parameters.cold_regime=1;
+
+
+parameters.n_chan=2;
+parameters.m=6;
+parameters.m_out=6;
+parameters.R_curv = R_min:R_step:R_max;
+results = iData(model,parameters);
+sum_R = sum(results, 0);
+
+fig=figure;
+plot(R_min:R_step:R_max,sum_R,'LineWidth',2);
+title([name ' scan R'])
+grid on
+xlabel('R, m')
+ylabel('I, arb.u.')
+legend('Curvature radius')
+print(gcf,[name 'R_scan'],'-dpng','-r300')
+%matlab2tikz([name 'm_scan.tex'], 'width', '0.85\textwidth');
+saveas(fig,[name 'R_scan.fig']);
 
