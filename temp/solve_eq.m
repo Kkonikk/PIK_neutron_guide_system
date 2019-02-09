da6ddf1dad25dd3763b0e87306a758e43e3a1e7b
karpenka <osnerde@gmail.com>
2019-02-09 15:20:58 +0300

    some mod

--- temp/solve_eq.m
+++ temp/solve_eq.m
@@ -1,6 +1,6 @@
 syms ls
 syms x
-a = solve(sqrt(8*0.05*x)/2+sqrt(ls^2+2*0.05*x)-ls-(26-ls)/1.15 == 0,x);
+a = solve(sqrt(8*0.03*x)/2+sqrt(ls^2+2*0.03*x)-ls-(25-ls)/1.15 == 0,x);
 for i = 1:25
     sum(i) = double(subs(a,ls,i));
 end
